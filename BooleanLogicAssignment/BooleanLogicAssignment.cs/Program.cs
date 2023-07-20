6ba0ced9f3b95771448b359f1fe7a0a248fb5c51
JamTerp79 <terpening.james@gmail.com>
2023-07-20 11:26:26 -0500

    Boolean Logic Assignment.

---
+++ BooleanLogicAssignment/BooleanLogicAssignment.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks the applicant their age
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            // Asks the applicant if they have ever had a DUI
+            Console.WriteLine("Have you ever had a DUI?  (true or false)");
+            bool hasDUI = bool.Parse(Console.ReadLine());
+
+            // Asks the applicant how many speeding tickets they have
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = int.Parse(Console.ReadLine());
+
+            // Creates a boolean expression to determine if the applicant qualifies for car insurance
+            bool qualified = age >= 15 && !hasDUI && speedingTickets <= 3;
+
+            // Prints the result of the boolean expression
+            Console.WriteLine("Qualified? {0}", qualified);
+
+            Console.ReadLine();
+        }
+    }
+}
