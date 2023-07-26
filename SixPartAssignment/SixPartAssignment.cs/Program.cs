cf63a31b16c416aabc2ac252d025203c9b81c737
JamTerp79 <terpening.james@gmail.com>
2023-07-26 11:22:05 -0500

    This is Part 1 of the assignment

---
+++ SixPartAssignment/SixPartAssignment.cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixPartAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array of strings
+            string[] stringArray = { "Hello", "World", "my", "name", "is" };
+
+            // Get user input
+            Console.Write("Enter some text: ");
+            string userInput = Console.ReadLine();
+
+            // Loop with index to modify array
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                stringArray[i] += userInput;
+            }
+
+            // Loop to print strings
+            foreach (string s in stringArray)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
